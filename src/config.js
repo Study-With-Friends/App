54fbc7764bafcc94c8af947ce6e7e78a0c76ffa3
Michael Pu <michael.pu123@gmail.com>
2020-08-22 17:40:19 -0400

    fix url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const localEndpoint = 'http://localhost:5000';
+// const localEndpoint = 'http://34.226.233.249';
 const prodEndpoint = 'http://google.com'
 
 const endpoint = process.env.NODE_ENV === 'production' ? prodEndpoint : localEndpoint;
