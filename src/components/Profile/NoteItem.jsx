a9137e3d6d13577b72091b10b1b21a82c2da09f5
Kelvin Zhang <kelvin.zhang@uwaterloo.ca>
2020-08-22 10:13:25 -0700

    fix: add missing noteitem

---
+++ src/components/Profile/NoteItem.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const NoteItem = styled.div`
+    display: flex;
+    flex: 1;
+    cursor: pointer;
+    justify-content: space-between;
+`;
+
+export default NoteItem;
